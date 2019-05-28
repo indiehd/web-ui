d017d0ccffd34ea0250718c184d9506e79697e49
poppabear8883 <servnx@gmail.com>
2019-05-28 13:27:42 -0400

    feat: Initial implementation for featured artists

---
+++ src/vuex/actions/Featured.js
@@ -0,0 +1,19 @@
+import axios from './axios';
+
+function FetchFeaturedArtists ({commit}) {
+  return new Promise((resolve, reject) => {
+    axios.get('/featured')
+      .then(resp => {
+        console.log(resp);
+        commit('SET_FEATURED_ARTISTS', resp.data);
+        resolve(resp.data);
+      })
+      .catch(err => {
+        reject(err);
+      });
+  });
+}
+
+export default {
+  FetchFeaturedArtists,
+};
